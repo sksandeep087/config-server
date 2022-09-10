f46ffd4400420d3c2c5692d7596ef354c4998054
sksandeep087 <34537939+sksandeep087@users.noreply.github.com>
2022-09-10 15:27:01 +0530

    Change ClientServerApplication  class

--- src/main/java/com/casas/server/ClientServerApplication.java
+++ src/main/java/com/casas/server/ClientServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServerApplication {
+public class ClientServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientServerApplication.class, args);
